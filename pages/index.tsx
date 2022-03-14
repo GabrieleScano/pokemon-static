9a86d6cd401b55d63c56674817b72fa10bb86df9
gabri <gabrielescano.bcn@gmail.com>
2022-03-14 18:31:36 +0100

    Add darkTheme basic setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import { Button } from '@nextui-org/react';
 
 
 const HomePage: NextPage = () => {
   return (
     <>
+    <Button>Ciao</Button>
     <h1>ciao</h1>
     
     </>

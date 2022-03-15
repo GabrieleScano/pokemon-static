07578e4fd4f60a2f8b4d0764458c1372c5e13630
gabri <gabrielescano.bcn@gmail.com>
2022-03-15 09:49:04 +0100

    fetch data and create pokemon interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import type { NextPage } from "next";
-import { Button } from "@nextui-org/react";
-import { Layout } from "../components/layouts/Layout";
+import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { Layout } from '../components/layouts/Layout'
+import pokeApi from '../api/pokeApi';
+import { PokemonListResponse } from '../interfaces/pokemon-list';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout title="List of Pokemons">
-      <Button color="gradient">Ciao</Button>
+
     </Layout>
   );
 };
 
-export default HomePage;
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+console.log(data)
+  return {
+    props: {
+      pokemons:data.results
+      
+    }
+  }
+}
+
+export default HomePage

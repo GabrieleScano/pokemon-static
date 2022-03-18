7de63d19846700325e6a47a0662b6d5bdd1de51a
gabri <gabrielescano.bcn@gmail.com>
2022-03-18 12:01:21 +0100

    Create css module for NavBar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,20 +1,15 @@
 import { Spacer, Text, useTheme } from '@nextui-org/react'
 import Image from 'next/image';
+import styles from './Navbar.module.css'
 
 export const Navbar = () => {
 
     const { theme } = useTheme()
   return (
-    <div style={{
-        display: 'flex',
-        flexDirection: 'row', 
-        justifyContent: 'start',
-        alignItems: 'center',
-        width: '100%',
-        padding: '0px, 20px',
-        backgroundColor: theme?.colors?.gray900.value,
-    
-    }}>
+    <div 
+      className={styles.logo}
+      style={{ backgroundColor: theme?.colors?.gray900.value}}
+    >
 
     <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="pokemon" width="70px" height="70px" />
     <Text color='white' h2>P</Text>

e5aff118abc47244888cefd0f32a13a55705ac5e
gabri <gabrielescano.bcn@gmail.com>
2022-03-21 18:04:13 +0100

    Save in localStorage favorites

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,77 +1,91 @@
+import { useState } from 'react'
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
 import { Button, Card, Container, Grid, Text, Image } from '@nextui-org/react'
 import { Layout } from '../../components/layouts/Layout'
 import pokeApi from '../../api/pokeApi'
 import { Pokemon } from '../../interfaces/pokemon-full'
 import styles from './pokemon.module.css'
+import { localFavorites } from '../../utils'
 
 interface Props{
   pokemon: Pokemon
 }
 const PokemonPage: NextPage<Props> = ({pokemon}) => {
 
+  const [isInFavorites, setIsInFavorites] = useState(localFavorites.existInFavorites(pokemon.id))
+
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id)
+    setIsInFavorites(!isInFavorites)
+  }
+  
+
   return (
     <Layout title={pokemon.name}>
-     
-        <Grid.Container gap={2} justify='flex-start'>
-          <Grid xs={12} sm={6} md={4} lg={3}>
-            <Card hoverable css={{padding: '30px'}}>
-              <Card.Body>
-                <Card.Image 
-                  src={pokemon.sprites.other?.dream_world.front_default || '/no-image.png'}
+      <Grid.Container gap={2} justify="flex-start">
+        <Grid xs={12} sm={6} md={4} lg={3}>
+          <Card hoverable css={{ padding: "30px" }}>
+            <Card.Body>
+              <Card.Image
+                src={
+                  pokemon.sprites.other?.dream_world.front_default ||
+                  "/no-image.png"
+                }
+                alt={pokemon.name}
+                width="100%"
+                height="200px"
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={6} md={8} lg={9}>
+          <Card hoverable className={styles.card}>
+            <Card.Header className={styles.card_header}>
+              <Text h1 transform="capitalize">
+                {pokemon.name}
+              </Text>
+              <Button
+                onClick={onToggleFavorite}
+                color="gradient"
+                ghost={!isInFavorites}
+              >
+                {isInFavorites ? "Remove from favorites" : "Add to favorites"}
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container display="flex" direction="row" gap={0}>
+                <Image
+                  src={pokemon.sprites.front_default}
                   alt={pokemon.name}
-                  width="100%" 
-                  height="200px"
+                  width={100}
+                  height={100}
                 />
-              </Card.Body>
-            </Card>
-          </Grid>
-          <Grid xs={12} sm={6} md={8} lg={9}>
-            <Card hoverable className={styles.card}>
-              <Card.Header className={styles.card_header}>
-                <Text h1 transform='capitalize'>
-                  {pokemon.name}
-                </Text>
-                <Button color="gradient" ghost>
-                  Save in favorites
-                </Button>
-
-              </Card.Header>
-              <Card.Body>
-                <Text size={30}>Sprites:</Text>
-                <Container display='flex' direction='row' gap={0}>
-                  <Image 
-                    src={pokemon.sprites.front_default} 
-                    alt={pokemon.name} 
-                    width={100}
-                    height={100}
-                  />
-                  <Image 
-                    src={pokemon.sprites.back_default} 
-                    alt={pokemon.name} 
-                    width={100}
-                    height={100}
-                  />
-                  <Image 
-                    src={pokemon.sprites.front_shiny} 
-                    alt={pokemon.name} 
-                    width={100}
-                    height={100}
-                  />
-                  <Image 
-                    src={pokemon.sprites.back_shiny} 
-                    alt={pokemon.name} 
-                    width={100}
-                    height={100}
-                  />
-                </Container>
-              </Card.Body>
-            </Card>
-          </Grid>
-          </Grid.Container>
-    
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
-  )
+  );
 }
 
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes

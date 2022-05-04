c04f80a60ce29bb871033f968276f0a97c7dd9c6
gabri <gabrielescano.bcn@gmail.com>
2022-05-04 11:44:33 +0200

    Incremental static generation for name

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -107,21 +107,33 @@ const PokemonByNamePage: NextPage<Props> = ({pokemon}) => {
 
     const { data } = await pokeApi.get<PokemonListResponse>(`/pokemon?limit=151`)
     const pokemonNames: string[] = data.results.map(pokemon => pokemon.name)
+    
     return {
       paths: pokemonNames.map((name) => (
         { params: { name } }
         )),
-      fallback: false
+      fallback: 'blocking'
     }
   }
   
   export const getStaticProps: GetStaticProps = async ({params}) => {
     
     const { name } = params as { name: string }
+    const pokemon = await getPokemonInfo(name)
+
+    if (!pokemon) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      }
+    }
+
 
     return {
       props: {
-        pokemon: await getPokemonInfo(name)
+        pokemon
       },
     }
   }
